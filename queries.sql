08bb0c916236841ef7572e7e66a72263675f77aa
Halo-Hue <2025164134@ub.edu.bz>
2026-05-07 13:20:04 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -26,3 +26,21 @@ GROUP BY ARTIST.District) AS COUNTS);
 SELECT SHOW.ShowName, SHOW.StartDate, SHOW.EndDate, SHOW.Price, VENUE.VenueName
 FROM SHOW JOIN VENUE ON SHOW.VenueID = VENUE.VenueID ORDER BY SHOW.Price DESC LIMIT 1;
 
+--Query 1: Highest-paid show an artist performed in
+SELECT a.ArtistName, s.ShowName, v.VenueName, s.StartDate, s.Price
+FROM ARTIST a
+JOIN SHOW_ARTISTS sa ON a.ArtistID = sa.ArtistID
+JOIN SHOW s ON sa.ShowID = s.ShowID
+JOIN VENUE v ON s.VenueID = v.VenueID
+WHERE s.Price = (SELECT MAX(Price) FROM SHOW);
+
+--Query 2: District that produced the most performing artists
+SELECT District, ArtistCount
+FROM (SELECT District, COUNT(ArtistID) AS ArtistCount
+FROM ARTIST
+GROUP BY District) AS DistrictStats
+WHERE ArtistCount = (SELECT MAX(CountPerDistrict) 
+FROM (SELECT COUNT(ArtistID) AS CountPerDistrict 
+FROM ARTIST 
+GROUP BY District) AS MaxSub
+);
